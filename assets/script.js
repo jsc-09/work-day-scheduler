b3a4eed8bb76e78eae435b97db635c59900197ce
Joanna Christian <joanna.s.christian@gmail.com>
2022-01-13 13:15:17 -0800

    worked on local storage

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,15 @@
 // Set current time
-let currentDay = moment().format('MMMM DD, YYYY, hh:mm:ss a');
-$("$currentDay").text(currentDay);
+let todayDate = moment().format("dddd, MMMM DD, YYYY");
+$("#currentDay").text(todayDate);
+
+let toDoListInput = document.getElementById('#to-do-list')
+let saveBtn = document.querySelector('.saveBtn')
+//Step 1: Save text content into local storage when save button is save.
+//Step 2: Add color for past (grayed out), present (red), future (green)
+
+saveBtn.addEventListener('click', saveData)
+
+function saveData(){
+    localStorage.setItem('#toDoListInput', toDoListInput.value);
+    console.log(toDoListInput);
+}
