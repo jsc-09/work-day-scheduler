31993471bc1a9a93865c23157a9168465befb500
Joanna Christian <joanna.s.christian@gmail.com>
2022-01-17 21:38:44 -0800

    completed saving and retriving hours and task functions

--- assets/script.js
+++ assets/script.js
@@ -2,46 +2,58 @@
 let todayDate = moment().format("dddd, MMMM DD, YYYY");
 $("#currentDay").text(todayDate);
 
-let saveBtn = document.querySelector('.saveBtn')
 
 let textBackground = document.querySelector('.text-background')
 //Step 1: Save text content into local storage when save button is save.
 //Step 2: Add color for past (grayed out), present (red), future (green)
 
-saveBtn.addEventListener('click', saveTask)
 
-//Add local storage store tasks
+/*
+saveBtn.forEach(button => {
+    button.addEventListener("click",saveTask)
+})*/
+let taskByHour = {}
 
-let tasksToday = []
+$('.saveBtn').each(function(){
+    $(this).click(saveTask);
+})
 
-function saveTask () {
-    let taskInput = $('input[name="task-input').val();
-    console.log(taskInput);
-    console.log(rowHour);
-    //Store Task in Local Storage
 
-    const taskByHour = {
-        hour: rowHour,
-        task: taskInput
-    }
+//Saving each task by hour
+function saveTask(event) {
+    // $(event.target).parent()
+    console.log($(this).parent());
+
+    let taskHour = $(this).parent().attr("id");
+    let taskTask = $(this).parent().children('textarea').val();
+    console.log(taskHour);
+    console.log(taskTask)
+
+    
+    taskByHour[taskHour] = taskTask
+    
     console.log(taskByHour);
 
-    tasksToday.push(taskByHour);
-    localStorage.setItem('tasksToday', JSON.stringify(tasksToday));
+    localStorage.setItem('taskByHour', JSON.stringify(taskByHour))
+ }
 
-    saveTask()
-}
+ //Retriving each task by hour
+ function init() {
+     let storedTask = JSON.parse(localStorage.getItem('taskByHour'));
+     console.log(storedTask)
+ }
+
 
 
 //set current hour to schedule hour 
 
 function toDoBackground (){
     const currentHour = moment().hour();
-    console.log(currentHour);
+   // console.log(currentHour);
 
     $('.time-block').each(function(){
        let rowHour = parseInt($(this).attr('id'))
-       console.log(rowHour);
+      // console.log(rowHour);
 
        if (rowHour === currentHour){
            $(this).children('textarea').addClass('present')
@@ -56,3 +68,4 @@ function toDoBackground (){
 }
 toDoBackground()
 
+init()
